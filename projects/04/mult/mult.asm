98530fe7d3efbd34219c2ecbfe42886e795f3e77
Masayukiii <szkmsyk041792@gmail.com>
2018-03-21 10:53:30 +0900

    defining cpu

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,25 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+    @R2
+    M=0
+    @R0
+    D=M
+    @END
+    D;JEQ
+    @R1
+    D=M
+    @END
+    D;JEQ
+(LOOP)
+    @R0
+    D=M
+    @R2
+    M=M+D
+    @R1
+    M=M-1
+    D=M
+    @LOOP
+    D;JGT
+(END)
